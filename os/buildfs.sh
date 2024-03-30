17fb22825cb662fcaaa4a945d3cd810314b4dd73
Fediory <f-he@163.com>
2024-03-30 23:40:47 +0800

    feat: rootfs-ubifs-ze.img

--- os/buildfs.sh
+++ os/buildfs.sh
@@ -1,5 +1,5 @@
 SUDO=$(if [ $(whoami) = "root" ];then echo -n "";else echo -n "sudo";fi)
-U_FAT32_DIR="./easy-fs-fuse"
+U_FAT32_DIR="../easy-fs-fuse"
 U_FAT32=$1
 BLK_SZ="512"
 TARGET=riscv64gc-unknown-none-elf
@@ -7,7 +7,7 @@ MODE="release"
 if [ $# -ge 2 ]; then
     if [ "$2"="2k1000" -o "$2"="laqemu" ]
     then
-        TARGET=loongarch64-linux-gnu
+        TARGET=loongarch64-unknown-linux-gnu
         BLK_SZ="2048"
     else
         TARGET=$2
@@ -24,7 +24,7 @@ echo
 echo Current arch: ${ARCH}
 echo
 "$SUDO" touch ${U_FAT32}
-"$SUDO" dd if=/dev/zero of=${U_FAT32} bs=1M count=50
+"$SUDO" dd if=/dev/zero of=${U_FAT32} bs=1M count=45
 echo Making fat32 image with BLK_SZ=${BLK_SZ}
 "$SUDO" mkfs.vfat -F 32 ${U_FAT32} -S ${BLK_SZ}
 "$SUDO" fdisk -l ${U_FAT32}
@@ -65,9 +65,9 @@ try_copy(){
     fi
 }
 
-for programname in $(ls ./user/src/bin)
+for programname in $(ls ../user/src/bin)
 do
-    "$SUDO" cp -r ./user/target/${TARGET}/${MODE}/${programname%.rs} ${U_FAT32_DIR}/fs/${programname%.rs}
+    "$SUDO" cp -r ../user/target/${TARGET}/${MODE}/${programname%.rs} ${U_FAT32_DIR}/fs/${programname%.rs}
 done
 
 if [ ! -f ${U_FAT32_DIR}/fs/syscall ]
@@ -76,9 +76,9 @@ then
 fi
 
 # try_copy ../user/user_C_program/user/build/${ARCH}  ${U_FAT32_DIR}/fs/syscall
-try_copy ./user/busybox_lua_testsuites/${ARCH} ${U_FAT32_DIR}/fs/
-try_copy ./user/${ARCH} ${U_FAT32_DIR}/fs/
-try_copy ./user/disk/${ARCH} ${U_FAT32_DIR}/fs/
+try_copy ../user/busybox_lua_testsuites/${ARCH} ${U_FAT32_DIR}/fs/
+try_copy ../user/${ARCH} ${U_FAT32_DIR}/fs/
+# try_copy ../user/disk/${ARCH} ${U_FAT32_DIR}/fs/
 
 "$SUDO" umount ${U_FAT32_DIR}/fs
 echo "DONE"
